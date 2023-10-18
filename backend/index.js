3f10a5dda01cf0620538b79348544dd37870778d
xskypi0 <xskypi0@gmail.com>
2023-10-17 21:17:58 -0600

    Administracion de Cors Policy

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,30 @@ import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js"
 
 const app = express();
+
 // Middleware for parsing request body
 app.use(express.json());
+
+//Middleware for handling cors POLICY check this reference https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+//option 1: Allow all origins with default of cors (*)
+//app.use(cors());
+//option 2: Allow custome cors 
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
+
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Primer proyecto MERN")
 });
 
-app.use('/books', booksRoute)
+app.use('/books', booksRoute);
 
 //conexion a mongoDBURL ubicado en ./config.js 
 mongoose
