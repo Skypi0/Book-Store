2b089f3c3d514c43a416b0161c134d614b994dfd
xskypi0 <xskypi0@gmail.com>
2023-10-17 18:23:48 -0600

    Agregamos ruta para buscar los libros por Id

--- backend/index.js
+++ backend/index.js
@@ -57,7 +57,23 @@ app.get('/books', async (request, response) => {
         console.log(error.message);
         response.status(500).send({ message: error.message });
     }
+}) 
+
+//Ruta para traer todos los libros de la base de datos por Id
+app.get('/books/:id', async (request, response) => {
+    try {
+        const {id} = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).send(book);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 })
+
 //conexion a mongoDBURL ubicado en ./config.js 
 mongoose
     .connect(mongoDBURL)
